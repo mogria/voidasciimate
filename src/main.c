08692f23f11e622847330f22887ab4b39c29c2d6
Mogria <m0gr14@gmail.com>
2012-09-19 18:15:26 +0200

    multiple text parts on 1 screen

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ int main() {
   int x, y;
   int ch;
   char **buffer;
-
+  char **part;
 
   srand(time(NULL));
 
@@ -28,15 +28,34 @@ int main() {
 
   getmaxyx(stdscr, size_y, size_x);
 
+  // allocate place for the buffer
   ALLOC2(char, buffer, size_y, size_x);
-  get_figlet_text(buffer, size_y, size_x, "VOID", "doh");
+
+
+  // welcome part
+  PART2(char, part, 20, buffer, 0, 0);
+  get_figlet_text(part, 20, size_x, "welcome", "doh");
+  FREE1(part);
+
+  // to the part
+  PART2(char, part, 10, buffer, 22, 0);
+  get_figlet_text(part, 10, size_x, "to the", "starwars");
+  FREE1(part);
+
+  // void part
+  PART2(char, part, 20, buffer, 30, 0);
+  get_figlet_text(part, 20, size_x, "VOID", "doh");
+  FREE1(part);
 
  
+  // fill screen with # characters
   for(y = 0; y < size_y; y++) {
     for(x = 0; x < size_x; x++) {
       mvprintw(y, x, "#");
     }
   }
+
+  // randomly draw bytes in buffer to correct position
   while(1) {
     ch = getch();
     x = (rand() / (float)RAND_MAX) * size_x;
@@ -51,5 +70,7 @@ int main() {
   }
 
   endwin();
-  return 0;
+
+  FREE2(buffer, size_y);
+  return EXIT_SUCCESS;
 }
