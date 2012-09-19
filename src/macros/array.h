ec8a41a330bbf348d5e5c5de5471b0d3b703f536
Mogria <m0gr14@gmail.com>
2012-09-19 18:14:34 +0200

    added possibility to extract create pointer to a part of an array (with shared memory)

--- src/macros/array.h
+++ src/macros/array.h
@@ -2,22 +2,40 @@
 #define _ARRAY_H
 
 
+// allocate memory for an 1 dimensional array
 #define ALLOC1(type, varname, size)  do { \
   varname = calloc((size), sizeof(type)); \
 } while(0)
 
+// allocate memory for an 2 dimensional array
 #define ALLOC2(type, varname, size1, size2)  do { \
-  varname = malloc(size1 * sizeof(type*));\
+  ALLOC1(type*, varname, size1); \
   int y; \
   for(y = 0; y < size1; y++) { \
     ALLOC1(type, varname[y], size2); \
   } \
 } while(0)
 
+// grab an part of an 1 dimensional array
+#define PART1(dest, source, offset) do {\
+  dest = &source[offset]; \
+} while(0)
+
+// grab an part of an 2 dimensional array
+#define PART2(type, dest, dest_size1, src, offset1, offset2) do {\
+  int i; \
+  ALLOC1(type*, dest, dest_size1); \
+  for(i = 0; i < dest_size1; i++) { \
+    PART1(dest[i], src[i + offset1], offset2); \
+  } \
+} while(0)
+
+// free memory of 1 dimensional array
 #define FREE1(varname)  do { \
   free(varname); \
 } while(0)
 
+// free memory of 2 dimensional array
 #define FREE2(varname, size) do { \
   int y = 0; \
   for(y = 0; y < size; y++) { \
